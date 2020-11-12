3dae0216bac662ac9abba370f83ac73bed83790d
Monoverde888 <minecraftersov@gmail.com>
2020-11-12 02:52:27 -0300

    añadir log

--- v15.sh
+++ v15.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "instalando nodejs v15 😎👊 "
-curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+mkdir $HOME.node-debian
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash - > $HOME.node-debian/v15.log
 sudo apt-get remove -y nodejs
 sudo apt-get install -y nodejs
