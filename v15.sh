c7656cb86ed4446e415df789ccd1e7e225306c37
Monoverde888 <minecraftersov@gmail.com>
2020-11-12 02:47:33 -0300

    arreglar

--- v15.sh
+++ v15.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "instalando nodejs v15 😎👊 "
-curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 sudo apt-get remove -y nodejs
 sudo apt-get install -y nodejs
